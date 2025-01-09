ea35a71c220511045e0ba893621277bb03e26bbd
Hikkiphisan <locphihuu@gmail.com>
2025-01-09 11:59:08 +0700

    fix playlist

--- src/main/java/com/codegym/model/Playlist.java
+++ src/main/java/com/codegym/model/Playlist.java
@@ -9,7 +9,7 @@ import java.util.Set;
 @Entity
 @Table(name = "playlist")
 @Data
-public class PlayList {
+public class Playlist {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/codegym/service_tamthoi/IPlayListService.java
+++
@@ -1,9 +0,0 @@
-package com.codegym.service_tamthoi;
-
-import com.codegym.model.PlayList;
-
-import java.util.List;
-
-public interface IPlayListService {
-
-}
