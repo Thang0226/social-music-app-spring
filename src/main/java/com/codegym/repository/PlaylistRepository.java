f0a91ef737286acce7c3f3fa4483aa906670d502
Hikkiphisan <locphihuu@gmail.com>
2025-01-09 14:05:19 +0700

    created CSS

--- src/main/java/com/codegym/repository/PlaylistRepository.java
+++ src/main/java/com/codegym/repository/PlaylistRepository.java
@@ -1,9 +1,22 @@
 package com.codegym.repository;
 
-import com.codegym.model.Playlist;
+import com.codegym.model.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
+public interface ISongRepository extends JpaRepository<Song, Long> {
+
+    @Query("SELECT s FROM Song s ORDER BY s.listeningCount DESC")
+    List<Song> findTopPlayedSongs();
+
+    @Query("SELECT s FROM Song s ORDER BY s.uploadTime DESC")
+    List<Song> findNewSongs();
+
+
+    @Query("SELECT s FROM Song s ORDER BY s.likeCount DESC")
+    List<Song> findTopLikedSongs();
 }
