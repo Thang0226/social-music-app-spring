0a8387424b522e82df5322ad8bdbfc024acfc822
Xuân Lộc <nguyenxuanloc.02042001@gmail.com>
2025-01-09 13:53:13 +0700

    playlist

--- src/main/java/com/codegym/repository/PlaylistRepository.java
+++ src/main/java/com/codegym/repository/PlaylistRepository.java
@@ -1,9 +1,9 @@
 package com.codegym.repository;
 
-import com.codegym.model.PlayList;
+import com.codegym.model.Playlist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PlayListRepository extends JpaRepository<PlayList, Long> {
+public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
 }
--- src/main/java/com/codegym/service/playList/IPlayListService.java
+++
@@ -1,6 +0,0 @@
-package com.codegym.service.playList;
-
-import com.codegym.model.PlayList;
-
-public interface IPlayListService extends IGenericService<PlayList> {
-}
