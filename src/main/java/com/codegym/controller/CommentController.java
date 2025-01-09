1ac73ed33b64bebb3030008069573e47a1cdff53
dunglt94 <tuandung9594@gmail.com>
2025-01-09 10:43:24 +0700

    add get song comment by id

---
+++ src/main/java/com/codegym/controller/CommentController.java
@@ -0,0 +1,26 @@
+package com.codegym.controller;
+
+import com.codegym.model.DTO.comment.SongCommentDTO;
+import com.codegym.service.comment.ICommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api")
+public class CommentController {
+    @Autowired
+    private ICommentService commentService;
+
+
+
+    @GetMapping("/song-comment/{id}")
+    public ResponseEntity<Page<SongCommentDTO>> getSongComment(
+            @PathVariable Long id, Pageable pageable) {
+        return new ResponseEntity<>(commentService.findCommentsBySongId(id, pageable), HttpStatus.OK);
+    }
+}
