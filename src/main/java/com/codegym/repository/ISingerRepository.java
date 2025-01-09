1a30cdadbf59bd8f00c319fdb49e0cf7a841c607
LeVanChuyen-20 <20222411@eaut.edu.vn>
2025-01-09 15:58:20 +0700

    fix

--- src/main/java/com/codegym/repository/ISingerRepository.java
+++ src/main/java/com/codegym/repository/ISingerRepository.java
@@ -3,8 +3,10 @@ package com.codegym.repository;
 
 import com.codegym.model.Singer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ISingerRepository extends JpaRepository<Singer, Long> {
+
 }
