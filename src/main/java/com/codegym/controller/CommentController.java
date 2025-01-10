ed724c4152c1af197443a328315439e48d38eaf5
dunglt94 <tuandung9594@gmail.com>
2025-01-10 11:31:26 +0700

    improve getComment ajax to show in page size of 10 and add functional load more button

--- src/main/java/com/codegym/controller/CommentController.java
+++ src/main/java/com/codegym/controller/CommentController.java
@@ -8,6 +8,7 @@ import com.codegym.service.comment.ICommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -30,7 +31,8 @@ public class CommentController {
 
     @GetMapping("/song-comment/{id}")
     public ResponseEntity<Page<SongCommentDTO>> getSongComment(
-            @PathVariable Long id, Pageable pageable) {
+            @PathVariable Long id,
+            @PageableDefault(size = 10) Pageable pageable) {
         return new ResponseEntity<>(commentService.findCommentsBySongId(id, pageable), HttpStatus.OK);
     }
 
