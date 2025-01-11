8cb964dc0ca0a57d2a1dbc71ee842463e6b02900
dunglt94 <tuandung9594@gmail.com>
2025-01-11 15:46:25 +0700

    add ajax to load comment for singer and playlist, improve create comment function

---
+++ client/js/singer.js
@@ -0,0 +1,82 @@
+let singerId = localStorage.getItem("singer-id");
+let userId = localStorage.getItem("user-id");
+
+let currentPage = 0;
+const pageSize = 10; // Number of comments per load
+
+function getSingerComment(singerId, isLoadMore = false) {
+    $.ajax({
+        headers: {
+            'accept': 'application/json',
+            'content-type': 'application/json',
+        },
+        url: `http://localhost:8080/api/singer-comment/${singerId}?page=${currentPage}&size=${pageSize}`,
+        type: 'GET',
+        success: function (data) {
+            console.log(data);
+            let comment = data.content;
+            let content = "";
+
+            for (let i = 0; i < comment.length; i++) {
+                let localTime = moment(comment[i].commentTime).tz("Asia/Ho_Chi_Minh").format("DD/MM/YYYY HH:mm");
+                content += `<div class="w-100 p-3 rounded-4 bg-light mb-3">
+                    <strong class="font-weight-bold text-primary">${comment[i].username}</strong><br>
+                    <span class="text-muted">${localTime}</span><br>
+                    <p class="mt-2 mb-0">${comment[i].content}</p>
+                </div>`
+            }
+
+            // If loading more, append to existing content
+            if (isLoadMore) {
+                $("#comments").append(content);
+            } else {
+                $("#comments").html(content);
+            }
+
+            // Show/hide load more button based on whether there are more comments
+            if (comment.length < pageSize) {
+                $("#load-more").hide();
+            } else {
+                $("#load-more").show();
+            }
+        }
+    })
+}
+
+// function to handle loading more comments
+function loadMore() {
+    currentPage++;
+    getSingerComment(singerId, true);
+}
+getSingerComment(singerId, false);
+
+function postComment() {
+    event.preventDefault();
+    let content = $("#comment-box").val().trim();
+    if (content === "") {
+        return;
+    }
+    let comment = {
+        user: {
+            id: userId
+        },
+        singer: {
+            id: singerId
+        },
+        content: content
+    };
+    $.ajax({
+        headers: {
+            'content-type': 'application/json',
+        },
+        url: 'http://localhost:8080/api/comments',
+        type: 'POST',
+        data: JSON.stringify(comment),
+        success: function (response) {
+            console.log(response)
+            getSingerComment(singerId, false);
+            $("#comment-box").val("")
+        }
+    })
+}
+
