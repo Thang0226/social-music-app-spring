0faa302eb6a81b3729e9becb86942beda787f4dd
Thang0226 <thang.nd0226@gmail.com>
2025-01-12 20:17:52 +0700

    clear bugs for listening count

--- client/js/song.js
+++ client/js/song.js
@@ -55,7 +55,6 @@ $(document).ready(function(){
                 </audio>
                 </div>`
             );
-            initializeMediaPlayers();
 
             // add listening count event
             $("#player2").on("play", function(){
@@ -211,52 +210,6 @@ function unlikeSong(songId) {
             $("#like-count").html(
                 `${parseInt(result, 10).toLocaleString('vi-VN')}`
             );
-        }
-    })
-}
-
-// MediaElement
-function initializeMediaPlayers() {
-    let mediaElements = document.querySelectorAll('video, audio'), total = mediaElements.length;
-
-    for (let i = 0; i < total; i++) {
-        new MediaElementPlayer(mediaElements[i], {
-            features: ['playpause', 'current', 'progress', 'duration', 'volume'],
-            pluginPath: 'https://cdn.jsdelivr.net/npm/mediaelement@7.0.7/build/',
-            shimScriptAccess: 'always',
-            success: function (mediaElement) {
-                let target = document.body.querySelectorAll('.player'), targetTotal = target.length;
-                for (let j = 0; j < targetTotal; j++) {
-                    target[j].style.visibility = 'visible';
-                }
-                // Increase view count after 30 seconds
-                mediaElement.addEventListener('timeupdate', function () {
-                    if (mediaElement.currentTime >= 30) {
-                        // Call your function to increase the view count
-                        increaseViewCount(songId);
-                        // Remove the event listener after it triggers once
-                        mediaElement.removeEventListener('timeupdate', arguments.callee);
-                    }
-                });
-            }
-        });
-    }
-}
-
-// Initialize players on DOMContentLoaded
-document.addEventListener('DOMContentLoaded', function () {
-    initializeMediaPlayers();
-});
-
-function increaseViewCount(songId) {
-    $.ajax({
-        headers: {
-            'content-type': 'application/json'
-        },
-        url: `${API_BASE_URL}/api/songs/update-listening-count/${songId}`,
-        type: 'POST',
-        success: function (result) {
-            $('#listening-count').html(`${parseInt(result, 10).toLocaleString('vi-VN')}`);
         }
     })
 }
