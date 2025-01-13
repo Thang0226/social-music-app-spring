04ea2f1d993043d3f0a5da332010b5849f25e588
dunglt94 <tuandung9594@gmail.com>
2025-01-13 14:26:54 +0700

    amend some code

--- client/js/index.js
+++ client/js/index.js
@@ -39,7 +39,7 @@ $(document).ready(function(){
                         <div class="d-block d-md-flex podcast-entry bg-white mb-3" data-aos="fade-up">
                           <div class="image" style="background-image: url('${API_BASE_URL}/images/${songs[i].imageFile}')">
                           </div>
-                          <div class="text">
+                          <div class="text w-100">
                             <h3 class="font-weight-light">
                               <a href="song.html" onclick="storeSongId(${songs[i].id}); storeUserId(userId)">
                                 ${songs[i].name}
@@ -73,42 +73,34 @@ $(document).ready(function(){
     });
 })
 
-$(document).ready(function () {
-    const playlistContainer = $(".featured-user .list-unstyled");
 
-    // Hàm gọi API để lấy danh sách playlist
-    function fetchPlaylist() {
-        $.ajax({
-            url: "http://localhost:8080/api/playlist", // URL của API
-            method: "GET",
-            dataType: "json",
-            success: function (data) {
-                // Xóa nội dung cũ
-                playlistContainer.empty();
-
-                // Lặp qua danh sách và thêm vào HTML
-                data.forEach(playlist => {
-                    const listPlaylist = `
-            <li>
-              <a href="playlist.html" class="d-flex align-items-center">
-<!--                <img src="${playlist.image}" alt="${playlist.name}" class="img-fluid mr-2">-->
-                <div class="podcaster">
-                  <span class="d-block" style="font-weight: bold">${playlist.name}</span>
-                  <span class="small">${playlist.listeningCount} lượt nghe</span>
-                </div>
-              </a>
-            </li>
-          `;
-                    playlistContainer.append(listPlaylist);
-                });
-            },
-            error: function (xhr, status, error) {
-                console.error("Error fetching playlist:", error);
-                playlistContainer.html("<p>Unable to load playlist. Please try again later.</p>");
+function getTopPlayedSongs() {
+    $.ajax({
+        url: "http://localhost:8080/api/homepage/top-played-songs", // URL của API
+        method: "GET",
+        success: function (data) {
+            let content = "";
+            for (let i = 0; i < 10; i++) {
+                content +=`
+                <li>
+                    <a href="playlist.html" class="d-flex align-items-center">
+                        <img src="${API_BASE_URL}/images/${data[i].imageFile}" 
+                        alt="${data[i].name}" class="img-fluid mr-2" 
+                        style="max-width: 50px; max-height: 50px; width: 100%; height: auto;">
+                        <div class="podcaster">
+                            <span class="d-block" style="font-weight: bold">${data[i].name}</span>
+                            <span class="small">
+                            ${parseInt(data[i].listeningCount, 10).toLocaleString('vi-VN')} lượt nghe
+                            </span>
+                        </div>
+                    </a>
+                </li>
+                `
             }
-        });
-    }
+            $("#popular-songs").html(content)
+        }
+    })
+}
 
-    // Gọi hàm fetchPlaylist khi trang tải
-    fetchPlaylist();
-});
+getTopPlayedSongs();
+
